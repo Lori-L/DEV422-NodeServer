2ae3ba0e93247a232ccc47fff506a1e4ef5d6d35
Gabriel Blake <61571088+gabrielblake28@users.noreply.github.com>
2023-10-14 16:01:31 -0700

    api test

--- server.ts
+++ server.ts
@@ -1,13 +1,22 @@
-const express = require("express");
-const path = require("path");
+import * as express from "express";
+import * as path from "path";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "src")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "src", "index.html"));
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(
+      "https://www.dnd5eapi.co/api/classes/paladin/levels?subclass=berserker"
+    );
+    console.log(result);
+    res.end();
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(port, () => {
