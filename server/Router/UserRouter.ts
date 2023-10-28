7b583ed06d8b10a4af622b3b378f70909a7295a6
bcuser <jacobsolvang@gmail.com>
2023-10-27 20:08:53 -0700

    General code clean up, removal of test method and removal of unused schema

--- server/Router/UserRouter.ts
+++ server/Router/UserRouter.ts
@@ -1,22 +1,10 @@
 import { Router } from "express";
-import { Connect } from "./connect";
 import { readWritePrimary } from "../../keys/keys";
 export const userRouter = Router();
 const { MongoClient } = require("mongodb");
-var ObjectId = require('mongodb').ObjectId;
-
 const client = new MongoClient(readWritePrimary, { useNewUrlParser: true });
 const db = client.db("dnddb");
-
-userRouter.get("/test", async (req, res) => {
-  try {
-    Connect();
-    res.send('Database Connected');
-  }
-  catch (error) {
-    res.send(error);
-  }
-});
+const ObjectId = require('mongodb').ObjectId;
 
 userRouter.get("/id?", async (req, res) => {
   try {
@@ -26,7 +14,7 @@ userRouter.get("/id?", async (req, res) => {
     res.send(result._id);
   }
   catch (err) {
-    res.send(err);
+    res.send({message: err});
   }
 });
 
@@ -36,26 +24,20 @@ userRouter.get("/password?", async (req, res) => {
         {username: req.query.username}
       );
     if (req.query.password == result.password) {
-      res.send({message: "true"});
+      res.send({message: "true",
+        _id: result._id});
     }
     else {
       res.send({message: "false"});
     }
   }
   catch (err) {
-    res.send(err);
+    res.send({message: err});
   }
 });
 
 userRouter.post("/signup?", async (req, res) => {
   try {
-    // const isUsernameTaken = req.body.username === "user"; // mongo function to check if user exists
-
-    // if (isUsernameTaken) {
-    //   res.send({ message: "User already exists" });
-    // } else {
-    //   res.send({ message: "Successfully signed up" }); // respond with error message "user already exists"
-    // }
     var result = await db.collection('users').findOne(
         {username: req.body.username}
       );
@@ -63,7 +45,6 @@ userRouter.post("/signup?", async (req, res) => {
       res.send({ message: "User already exists" });
     }
     else {
-      console.log("Trying");
       var user = {
         _id: new ObjectId(),
         username: req.body.username,
@@ -72,21 +53,11 @@ userRouter.post("/signup?", async (req, res) => {
         usersShard: 0
       };
       db.collection('users').insertOne(user);
-      res.send({ message: "Successfully signed up" });
-      console.log("Submitted");
+      res.send({ message: "Successfully signed up",
+        id: user._id });
     }
   }
-  catch (error) {
-    res.send(error);
-  }
-});
-
-userRouter.delete("/", async (req, res) => {
-  try {
-    const result = ""; // mongo function to delete user
-    res.send(result);
-  } 
-  catch (error) {
-    res.send(error);
+  catch (err) {
+    res.send({message: err});
   }
 });
